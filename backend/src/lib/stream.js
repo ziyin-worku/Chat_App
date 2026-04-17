66e177893912d7cd130118503849bd6e286dad05
ziyin worku <ziyinworku@gmail.com>
2026-04-17 15:59:43 +0300

    chat-route and chat-controller  created

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -19,5 +19,12 @@ export const upsertStreamUser = async (userData) => {
   }
 };
 
-// to do later
-export const generateStreamToken = (userId) => {};
+export const generateStreamToken = (userId) => {
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error generating Stream token:", error);
+  }
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
+import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getRecommendedUsers,
   getMyFriends,
